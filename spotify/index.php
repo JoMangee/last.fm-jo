2d0f77adeef8240e039a359884f54d41938d8e3d
Jo <17394+JoMangee@users.noreply.github.com>
2026-03-21 23:24:57 +1300

    Add user-read-private scope to fix unknown display name

--- spotify/index.php
+++ spotify/index.php
@@ -21,7 +21,7 @@ if (isset($_GET['connect'])) {
         $state = bin2hex(random_bytes(16));
         $_SESSION['spotify_oauth_state'] = $state;
         $authUrl = spotify_get_auth_url($config['client_id'], $config['redirect_uri'], $state,
-            'user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing'
+            'user-read-private user-top-read user-read-playback-state user-modify-playback-state user-read-currently-playing'
         );
         header('Location: ' . $authUrl);
         exit;
